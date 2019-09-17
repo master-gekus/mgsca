15d6d54a11e662f23d4472a8a3bb705c6a30f6f5
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2019-09-17 11:10:24 +0300

    Added OpenSSL

--- main_window.cpp
+++ main_window.cpp
@@ -1,11 +1,15 @@
 #include "main_window.h"
 #include "./ui_main_window.h"
 
+#include <openssl/ssl.h>
+
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)
   , ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+
+  ::SSL_library_init();
 }
 
 MainWindow::~MainWindow()
